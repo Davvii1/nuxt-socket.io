ae605226664f24f2b2adef964fa2b694ac029f13
Davii1 <david2002rg@hotmail.com>
2024-01-17 09:41:06 -0600

    change package

--- preset/nitro.config.ts
+++ preset/nitro.config.ts
@@ -2,7 +2,7 @@ import type { NitroPreset } from "nitropack"
 import { fileURLToPath } from "node:url"
 
 export default <NitroPreset>{
-  extends: "netlify", // You can extend existing presets
+  extends: "node", // You can extend existing presets
   entry: fileURLToPath(new URL("./entry.ts", import.meta.url)),
   serveStatic: true,
   hooks: {
